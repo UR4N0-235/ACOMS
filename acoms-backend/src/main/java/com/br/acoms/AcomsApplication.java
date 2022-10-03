a20b9603790855d16e436510632b93c75bd9ef40
Matheus Fernandes <matheus.fernandes92.235@protonmail.com>
2022-10-03 18:09:08 -0300

    initial commit

---
+++ acoms-backend/src/main/java/com/br/acoms/AcomsApplication.java
@@ -0,0 +1,13 @@
+package com.br.acoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcomsApplication.class, args);
+	}
+
+}
---
+++ acoms-backend/src/test/java/com/br/acoms/AcomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.acoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
