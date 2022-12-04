af424f80421a02c45ea6db26d9bf07d730c3d11b
Matheus Fernandes <matheus.fernandes92.235@protonmail.com>
2022-12-04 14:16:50 -0300

    added guardian getinfos

--- acoms/integrationFile/isolateControllers/guardian.js
+++ acoms/integrationFile/isolateControllers/guardian.js
@@ -1,24 +1,45 @@
-$(document).ready(runController());
+runController();
 
-function runController(){
-    getLoggedGuardianInfos();    
+function runController() {
+    getLoggedGuardianInfos();
 }
 
-async function getLoggedGuardianInfos(){
+async function getLoggedGuardianInfos() {
+    console.log("logged test");
     let jwt = getAuthCookie();
-    let url = `${getIpServer()}:8080/guardian`;
+    let url = `http://${getIpServer()}:8080/guardian`;
     var header = {
         'method': "get",
         headers: {
+            'Content-Type': 'application/json',
             'Authorization': `Bearer ${jwt}`
         }
     }
-    console.log(`header ${JSON.stringify(header)}`);
-    //header.headers = { 'Content-Type': 'application/json' };
-    const request = await fetch(url, header);
-    const recived = request.json();
-    recived.then((response) => {
-        console.log(response)
+    console.log(JSON.stringify(header));
+    await fetch(url, header).then(async (response) => {
+        if (response.status == 200) {
+            loggedGuardian = await response.json();
+            defineFields(loggedGuardian);
+            console.log("logged is" + loggedGuardian);
+        } else {
+            console.log("deu error" + response.status);
+            window.location.replace("http://127.0.0.1:5500/paginas/landingpage/landingpage.html");
+        }
+    }).catch((err) =>{
+        console.log("error " + err);
+        window.location.replace("http://127.0.0.1:5500/paginas/landingpage/landingpage.html"); 
     });
-    
+
+    console.log("ue, acabo");
+}
+
+function defineFields(loggedGuardian) {
+    $(".guardianName").each((index, guardian) => {
+        $(guardian).text(loggedGuardian.name);
+    });
+
+    $(".schoolName").each((index, school) => {
+        $(school).text(loggedGuardian.school.name);
+    });
+
 }
